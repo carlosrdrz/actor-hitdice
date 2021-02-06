207170a60cdf3f25bb687dde25b1799267f49281
Carlos Rodriguez <crresse@gmail.com>
2021-02-06 00:42:44 +0000

    small fixes here and there to make this work. version 0.1.1.

--- scripts/actor-hitdice.js
+++ scripts/actor-hitdice.js
@@ -1,14 +1,10 @@
 export default class ActorHitdice {
     constructor() {
-        this.counter = {};
-        this._rollTableList = {};
-        this.dict = null;
         this.initHooks();
     }
 
     initHooks() {
         this.registerSettings();
-        this.loadSettings();
 
         Hooks.on('ready', async () => {
             if (!game.user.isGM)
@@ -39,26 +35,29 @@ export default class ActorHitdice {
 
 
     _rollHP(data, actor) {
-        const actorHitdiceEnabled = actor.data.data.attributes[this.attr_enabler];
-        const hitdice = actor.data.data.attributes[this.attr_hitdice];
+        const attr_enabler = game.settings.get("Actor-Hitdice", 'hitdice-enabled-attr');
+        const attr_hitdice = game.settings.get("Actor-Hitdice", 'hitdice-attr');
+        const attr_hp = game.settings.get("Actor-Hitdice", 'hp-attr');
+        const actorHitdiceEnabled = actor.data.data.attributes[attr_enabler];
+        const hitdice = actor.data.data.attributes[attr_hitdice];
 
         if (actorHitdiceEnabled != undefined &&
             actorHitdiceEnabled.value == true &&
             hitdice != undefined) {
             const r = new Roll(hitdice.value.replace(" ", ""));
             r.roll();
-            if (this.data.hp.toChat)
-                r.toMessage({
-                    rollMode: "gmroll",
-                    flavor: data.name + " rolls for hp!"
-                });
+            r.toMessage({
+                rollMode: "gmroll",
+                flavor: data.name + " rolls for hp!"
+            });
+            
             // Make sure hp is at least 1
             const val = Math.max(r.total, 1);
-            const hpAttr = "actorData.data.attributes." + this.attr_hp;
+            const hpAttr = "actorData.data.attributes." + attr_hp;
             setProperty(data, hpAttr + ".value", val);
             setProperty(data, hpAttr + ".max", val);
         } else
-            ui.notifications.warn("Can not randomize hp. HP formula is not set.");
+            ui.notifications.warn("Actor Hitdice error: can not randomize HP. Cant find required attributes.");
         return;
     }
 
@@ -69,10 +68,7 @@ export default class ActorHitdice {
             hint: "The name of a boolean actor attribute that will decide if actor-hitdice should run for that actor.",
             default: "actor_hitdice_enabled",
             type: String,
-            scope: 'world',
-            onChange: data => {
-                this.attr_enabler = data;
-            }
+            scope: 'world'
         });
 
         game.settings.register("Actor-Hitdice", 'hitdice-attr', {
@@ -80,10 +76,7 @@ export default class ActorHitdice {
             hint: "The name of a string actor attribute that specifies the roll required to get the hit points of the actor.",
             default: "hitdice",
             type: String,
-            scope: 'world',
-            onChange: data => {
-                this.attr_hitdice = data;
-            }
+            scope: 'world'
         });
 
         game.settings.register("Actor-Hitdice", 'hp-attr', {
@@ -91,10 +84,7 @@ export default class ActorHitdice {
             hint: "The name of a int actor attribute that specifies the hp of the actor, which will be modified by the module. It should have a max value too.",
             default: "hp",
             type: String,
-            scope: 'world',
-            onChange: data => {
-                this.attr_hp = data;
-            }
+            scope: 'world'
         });
     }
 }
